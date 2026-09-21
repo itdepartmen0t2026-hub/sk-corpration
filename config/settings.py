2014ca0ccd7e12cbc11d6864955b452cbfeb4f7e
itdepartmen0t2026-hub <itdepartmen0t2026@gmail.com>
2026-09-21 17:24:38 +0530

    first commitff

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-sk-corporation-local-key")
 DEBUG = os.getenv("DJANGO_DEBUG", "1") == "1"
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sk-corpration.onrender.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
